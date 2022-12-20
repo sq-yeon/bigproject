bf5cb256fe40832dd015a01b929999cff42be39c
sq-yeon <112459554+sq-yeon@users.noreply.github.com>
2022-12-20 14:47:30 +0900

    Update rain.py

--- rain.py
+++ rain.py
@@ -338,7 +338,7 @@ def building_searching():
     st.title('시설물 검색')
     title = st.text_input('시설물 검색','도로명 주소(시설명)을 입력하세요.')
     
-   if ((title == '코엑스') | ('강남구 영동대로 513' in title) ):
+   if ((title == '코엑스') | ('강남구 영동대로 513' in title)):
         st.header('검색 결과')
         df = seoul.loc[(seoul['시설명']=='코엑스')]
         st.map(df, zoom=13)
